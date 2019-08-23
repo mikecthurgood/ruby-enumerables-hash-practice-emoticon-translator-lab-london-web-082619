9b2c6abe19692665b6ec10d0a8976ad8d713b170
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-23 09:25:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,3 +11,13 @@ end
 def get_english_meaning
   # code goes here
 end
+
+
+
+require "yaml"
+
+def load_library(emoticon_file)
+  emoticons = YAML.load_file("./lib/emoticons.yml")
+  
+  emoticon_lib = {"get_meaning" => {},
+                  "get_emoticon" => {}}
