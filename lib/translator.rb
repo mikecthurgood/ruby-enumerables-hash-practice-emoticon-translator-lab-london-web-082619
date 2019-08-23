aa7fc8726c3cd478cad4ad5e0c1b56be8cb0e74a
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-23 09:26:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,15 @@ def load_library(emoticon_file)
   
   emoticon_lib = {"get_meaning" => {},
                   "get_emoticon" => {}}
+                  
+  emoticons.each do |meaning, value|
+  english = value[0]
+  japanese = value[1]
+  emoticon_lib["get_meaning"][japanese] = meaning
+  emoticon_lib["get_emoticon"][english] = japanese
+  end
+  emoticon_lib
+end
 end
 
 def get_japanese_emoticon
